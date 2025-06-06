0069552983067f36a4167937d01820f82c062fd8
NicolasBondenet <36343149+Vaquicraft@users.noreply.github.com>
2025-06-06 11:04:45 +0200

    update

--- vaqui/src/main.ts
+++ vaqui/src/main.ts
@@ -1,25 +1,61 @@
 import './style.css'
-import typescriptLogo from './typescript.svg'
-import viteLogo from '/vite.svg'
-import { setupCounter } from './counter.ts'
-import "./variable.ts"
 
-document.querySelector<HTMLDivElement>('#app')!.innerHTML = `
-  <div>
-    <a href="https://vite.dev" target="_blank">
-      <img src="${viteLogo}" class="logo" alt="Vite logo" />
-    </a>
-    <a href="https://www.typescriptlang.org/" target="_blank">
-      <img src="${typescriptLogo}" class="logo vanilla" alt="TypeScript logo" />
-    </a>
-    <h1>Vite + TypeScript</h1>
-    <div class="card">
-      <button id="counter" type="button"></button>
-    </div>
-    <p class="read-the-docs">
-      Click on the Vite and TypeScript logos to learn more
-    </p>
-  </div>
-`
+const button = document.getElementById("inputButton")!
 
-setupCounter(document.querySelector<HTMLButtonElement>('#counter')!)
+let container = document.createElement("div")
+container.id = "container"
+
+let liste = []
+
+button.addEventListener("click" ,function() {
+  let input = document.getElementById("inputText") as HTMLInputElement
+  liste.push(input.value)
+  console.log(liste)
+
+  container.textContent = ""
+  
+  document.body.appendChild(container)
+
+  for (let i = 0; i < liste.length ; i++) {
+        
+        let ticktickDiv = document.createElement("div")
+        ticktickDiv.id = "ticktickdiv"
+        container.appendChild(ticktickDiv)
+    
+    
+        let listElement = document.createElement("div")
+        listElement.id = "listElement"
+
+
+        let checkbox = document.createElement("input")
+        checkbox.type = "checkbox"
+        checkbox.id = `ticktickDiv.${i}`
+        checkbox.name = "test"
+        checkbox.value = "off"
+        listElement.textContent = (i+1) +  " : " + liste[i]
+
+        ticktickDiv.appendChild(listElement)
+        ticktickDiv.appendChild(checkbox)
+        
+  }
+
+})
+
+
+//container
+
+  //div
+      //checkbox
+      //listElement
+
+  //div
+      //checkbox
+      //listElement
+
+      //div
+      //checkbox
+      //listElement
+
+      //div
+      //checkbox
+      //listElement
--- vaqui/src/variable.ts
+++ vaqui/src/variable.ts
@@ -1,39 +1,39 @@
-const mastring: string = "coucou"
+// const mastring: string = "coucou"
 
-function maFonction(a: string, b: number, c: string): string {
-    console.log("a => ", a)
-    console.log("b => ", b)
-    console.log("c => ", c)
-    return a
-}
+// function maFonction(a: string, b: number, c: string): string {
+//     console.log("a => ", a)
+//     console.log("b => ", b)
+//     console.log("c => ", c)
+//     return a
+// }
 
-const name = maFonction("john", 40, mastring)
+// const name = maFonction("john", 40, mastring)
 
-console.log("name de la fonction => ", name)
+// console.log("name de la fonction => ", name)
 
-interface VoitureInterface {
-    toto: string,
-    marque: string,
-    modele: string,
-    annee: number,
-    couleur: string
-}
+// interface VoitureInterface {
+//     toto: string,
+//     marque: string,
+//     modele: string,
+//     annee: number,
+//     couleur: string
+// }
 
-interface NewInterface extends VoitureInterface {
-    coucou: string
-}
+// interface NewInterface extends VoitureInterface {
+//     coucou: string
+// }
 
-const voiture: NewInterface = {
-    coucou: "coucou",
-    toto: "toto",
-    marque: "Renault",
-    modele: "Clio",
-    annee: 2020,
-    couleur: "rouge"
-}
+// const voiture: NewInterface = {
+//     coucou: "coucou",
+//     toto: "toto",
+//     marque: "Renault",
+//     modele: "Clio",
+//     annee: 2020,
+//     couleur: "rouge"
+// }
 
-function afficherVoiture(voiture: VoitureInterface): void {
-    console.log(`Voiture : ${voiture.toto} ${voiture.marque} ${voiture.modele}, Année : ${voiture.annee}, Couleur : ${voiture.couleur}`);
-}
+// function afficherVoiture(voiture: VoitureInterface): void {
+//     console.log(`Voiture : ${voiture.toto} ${voiture.marque} ${voiture.modele}, Année : ${voiture.annee}, Couleur : ${voiture.couleur}`);
+// }
 
-afficherVoiture(voiture)
+// afficherVoiture(voiture)
